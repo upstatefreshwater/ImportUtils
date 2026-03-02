2751c40df18df5074516efc159517e7c33b2bded
David Andrews <dsandrew@upstatefreshwater.org>
2026-03-02 16:13:18 -0500

    add troll column names to internal data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,11 +1,19 @@
 
 trollCOMM_serials <- c(1151975,1153542)
 
+troll_parameter_colnames <- c("Temperature (°C)", "Specific Conductivity (µS/cm)", "pH (pH)", "pH mV (mV)",
+                        "RDO Concentration (mg/L)", "RDO Saturation (%Sat)", "Turbidity (NTU)", "ORP (mV)",
+                        "Chlorophyll-a Fluorescence (RFU)", "BGA-PC Fluorescence (RFU)")
+
+troll_metadata_colnames <- c("Pressure (psi)","Latitude (°)", "Longitude (°)", "Marked", "Date Time", "Depth (m)")
+
+allposs_troll_colnames <- c(troll_metadata_colnames,troll_parameter_colnames)
 
 usethis::use_data(
-  badparams,
-  dup_patterns,
-  lod_checkdata,
+  trollCOMM_serials,
+  troll_parameter_colnames,
+  troll_metadata_colnames,
+  allposs_troll_colnames,
   internal = TRUE,
   overwrite = TRUE
 )
