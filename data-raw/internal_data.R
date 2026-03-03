bc6bafa30ff9657c12cf1723d02598824a4e51ff
David Andrews <dsandrew@upstatefreshwater.org>
2026-03-03 10:39:40 -0500

    WIP: commit to merge SH edits from main

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -2,32 +2,33 @@
 trollCOMM_serials <- c(1151975,1153542)
 
 troll_column_dictionary <- tibble::tribble(
-  ~pattern,                                  ~canonical,                ~required,
+  ~pattern,                                  ~canonical,                ~required,   ~meta,
 
   # Required core
-  "^Date Time$",                              "DateTime",                TRUE,
-  "^Depth \\(m\\)$",                          "depth_m",                 TRUE,
+  "^Date Time$",                              "DateTime",                TRUE,      FALSE,
+  "^Depth \\(m\\)$",                          "depth_m",                 TRUE,      FALSE,
 
   # Core parameters
-  "^Specific Conductivity",                   "sp_conductivity_uScm",    FALSE,
-  "^Temperature \\(°C\\)$",                   "temperature_C",           FALSE,
-  "^pH \\(pH\\)$",                            "pH_units",                FALSE,
-  "^pH mV",                                   "pH_mV",                   FALSE,
-  "^RDO Concentration",                       "DO_mgL",                  FALSE,
-  "^RDO Saturation",                          "DO_per",                  FALSE,
-  "^Turbidity",                               "turbidity_NTU",           FALSE,
-  "^Chlorophyll-a Fluorescence",              "chlorophyll_RFU",         FALSE,
-  "^BGA-PC Fluorescence",                     "bga_fluorescence_RFU",    FALSE,
-  "^Depth to Water",                          "depth_to_water_m",        FALSE,
+  "^Specific Conductivity",                   "sp_conductivity_uScm",    FALSE,     FALSE,
+  "^Temperature \\(°C\\)$",                   "temperature_C",           FALSE,     FALSE,
+  "^pH \\(pH\\)$",                            "pH_units",                FALSE,     FALSE,
+  "^pH mV",                                   "pH_mV",                   FALSE,     FALSE,
+  "^RDO Concentration",                       "DO_mgL",                  FALSE,     FALSE,
+  "^RDO Saturation",                          "DO_per",                  FALSE,     FALSE,
+  "^Turbidity",                               "turbidity_NTU",           FALSE,     FALSE,
+  "^Chlorophyll-a Fluorescence",              "chlorophyll_RFU",         FALSE,     FALSE,
+  "^BGA-PC Fluorescence",                     "bga_fluorescence_RFU",    FALSE,     FALSE,
+  "^Depth to Water",                          "depth_to_water_m",        FALSE,     FALSE,
 
   # Instrument metadata
-  "^External Voltage",                        "external_voltage_V",      FALSE,
-  "^Battery Capacity",                        "battery_capacity_per",    FALSE,
-  "^Barometric Pressure",                     "barometric_pressure_mbar",FALSE,
-  "^Latitude",                                "latitude_deg",            FALSE,
-  "^Longitude",                               "longitude_deg",           FALSE,
-  "^Marked$",                                 "marked_flag",             FALSE,
-  "^Trollcom_temperature_C$",                 "Trollcom_temperature_C",  FALSE
+  "^External Voltage",                        "external_voltage_V",      FALSE,     TRUE,
+  "^Battery Capacity",                        "battery_capacity_per",    FALSE,     TRUE,
+  "^Barometric Pressure \\(mbar\\)",          "barometric_pressure_mbar",FALSE,     TRUE,
+  "^Barometric Pressure \\(mm Hg\\)",         "barometric_pressure_mmHg",FALSE,     TRUE,
+  "^Latitude",                                "latitude_deg",            FALSE,     TRUE,
+  "^Longitude",                               "longitude_deg",           FALSE,     TRUE,
+  "^Marked$",                                 "marked_flag",             FALSE,     TRUE,
+  "^Trollcom_temperature_C$",                 "Trollcom_temperature_C",  FALSE,     TRUE
 )
 
 usethis::use_data(
